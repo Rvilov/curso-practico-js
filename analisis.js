d631046e97a2eca8c6692aaf777e268ea3906279
Ricardo Viloria <ricadovilov@gmail.com>
2022-07-20 20:11:12 -0400

    Se agregaron INPUTS de taller 4 salarios

--- analisis.js
+++ analisis.js
@@ -1,56 +1,108 @@
 
+var lista = [
+];
 
 
-const salariosVen = venezuela.map(
-    function(persona) {
-        return persona.salary;
-    }
-);
 
-const salariosVenOrden = salariosVen.sort(function(salaryA,salaryB){
-    return  salaryA-salaryB;    
-});
+function agregarSalario(){
+    const name = document.getElementById("nombre");
+    const salary = document.getElementById("numero");
+    let formSal = document.getElementById("salarios");
+    lista.push({
+        name: name.value,
+        salary: Number(salary.value),
+    });
+    console.log(lista);
+    name.focus();
+    formSal.reset();
+};
+
+
+
 
 //helpers
 function esPar(numero){
     return (numero %2 === 0); 
 };
 
-function calcularPromedioLista1(lista){
+function calcularPromedioLista1(lista1){
  
-    const sumaLista = lista.reduce(function(valorAcumulado = 0, elemento){
+    const sumaLista = lista1.reduce(function(valorAcumulado = 0, elemento){
         return valorAcumulado + elemento;
     });
 
-    let promedioLista = sumaLista/lista.length;
+    let promedioLista = sumaLista/lista1.length;
     return promedioLista;
 };
 
-//calculadora mediana
-function medianaSalarios(lista){
-    const mitad = parseInt(lista.length/2);
+function medianaSalarios(){
+    const salariosVen = lista.map(
+        function(persona) {
+            return persona.salary;
+            
+        }
+    );
+    const salariosVenOrden = salariosVen.sort(function(salaryA,salaryB){
+        return  salaryA-salaryB;    
+    });
 
+    medianaSalarios1(salariosVenOrden);
+};
+
+//calculadora mediana
+function medianaSalarios1(lista){
+
+   
+
+    const mitad = parseInt(lista.length/2);
+    
     if(esPar(lista.length)){
         const personaMitad = lista[mitad - 1];
         const personaMitad2= lista[mitad];
         const mediana = calcularPromedioLista1([personaMitad,personaMitad2]);
+         
+        result.innerHTML =`
+        <h2>La mediana es: ${mediana}</h2>   
+        `;
         return mediana;
     }else{
         const personaMitad = lista[mitad];
-        return personaMitad;
+        result.innerHTML =`
+        <h2>La mediana es: ${personaMitad}</h2>   
+        `;
+         return personaMitad;
     }
 };
 
-const medianaGeneralVen = medianaSalarios(salariosVenOrden)
+function medianaSalariosTop10(){
+    if(lista.length<10){
+        result.innerHTML =`
+        <h2>Para calcular el top 10 debe ingresar al menos 10 elementos</h2>   
+        `;
+    }else{
+    
+    const salariosVen = lista.map(
+        function(persona) {
+            return persona.salary;
+            
+        }
+    );
+    const salariosVenOrden = salariosVen.sort(function(salaryA,salaryB){
+        return  salaryA-salaryB;    
+    });
+    console.log("salariosvenorden: " +salariosVenOrden);
+    const spliceStart = salariosVenOrden.length * 0.9;
+    
+    const spliceCount = salariosVenOrden.length - spliceStart;
+    
+    const salariosTop10Ven = salariosVenOrden.splice(spliceStart,spliceCount);
+    console.log("salariosvto10: " +salariosTop10Ven)
+    let medianaTop10Ven = medianaSalarios1(salariosTop10Ven);
+    result.innerHTML =`
+        <h2>La mediana del top 10 es: ${medianaTop10Ven}</h2>   
+        `;
+    }
+};
 
-const spliceStart = salariosVenOrden.length * 0.9;
-const spliceCount = salariosVenOrden.length - spliceStart;
 
 
-const salariosTop10Ven = salariosVenOrden.splice(spliceStart,spliceCount);
-const medianaTop10Ven = medianaSalarios(salariosTop10Ven);
-
-console.log({
-    medianaGeneralVen,
-    medianaTop10Ven,
-});
